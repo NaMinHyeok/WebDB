aaa6d996e6af9bb55ce9f93b7f7791afb5008d71
naminhyeok <nmh9097@gmail.com>
2022-10-05 13:31:34 +0900

    table 안에 author에 대한 정보가 출력되도록 쿼리문 생성

--- WebDB2022_09_21/lib/author.js
+++ WebDB2022_09_21/lib/author.js
@@ -6,81 +6,26 @@ var url=require('url');
 module.exports =  {
     home : function(response){
         db.query(`SELECT * FROM topic`, function(error, topics){
-            var title = 'Welcome'
-            var description = 'Hello, Node.js'
-            var list = template.list(topics);
-            var html = template.HTML(title,list,
-                `<table>
-                    <tr><td></td></tr>
-                </table>
-                <style>
-                    table { border-collapse: collapse;}
-                    td { border: 1px solid black;}
-                </style>
-                `,``
-                );
-            response.writeHead(200);
-            response.end(html);
-        });
-    },
-
-    page : function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    db.query(`SELECT * FROM topic`, function(error, topics){
-        if(error){
-            throw error;
-        }
-        db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,[queryData.id],function(error2,topic){
-            if(error2){
-                throw error2;
-            }
-            var title = topic[0].title;
-            var description = topic[0].description;
-            var list = template.list(topics);
-            var html = template.HTML(title, list,
-                `<h2>${title}</h2>
-                ${description}
-                <p>by ${topic[0].name}</p>
-                `,
-                `<a href="/create">create</a> <a href="/update?id=${queryData.id}">update</a>
-                    <form action="delete_process" method="post">
-                        <input type="hidden" name="id" value="${queryData.id}">
-                        <input type="submit" value="delete">
-                    </form>`
-                );
-                response.writeHead(200);
-                response.end(html);
-        });
-    });
-    },
-    create : function(request,response){
-        db.query(`SELECT * FROM topic`, function(error, topics) {
-            db.query('SELECT * FROM author', function(error2, authors){
-                var title = 'Create';
+            db.query(`SELECT * FROM author` ,function(error2,authors){
+                var title = 'Welcome'
+                var description = 'Hello, Node.js'
                 var list = template.list(topics);
-                var html = template.HTML(title, list,
-                    `
-                    <form action="/create_process" method="post">
-                            <p><input type="text" name="title" placeholder="title"></p>
-                            <p>
-                                <textarea name="description" placeholder="description"></textarea>
-                            </p>
-                            <p>
-                                ${template.authorSelect(authors,'')}
-                            </p>
-                            <p>
-                                <input type="submit">
-                            </p>
-                    </form>
-                    `,
-                    `<a href="/create">create</a>`
-                );
+                var html = template.HTML(title,list,
+                    `<table>
+                        <tr><td></td></tr>
+                    </table>
+                    <style>
+                        table { border-collapse: collapse;}
+                        td { border: 1px solid black;}
+                    </style>
+                    `,``
+                    );
                 response.writeHead(200);
                 response.end(html);
             });
         });
     },
+
     create_process : function(request,response){
         var body ='';
         request.on('data',function(data){
